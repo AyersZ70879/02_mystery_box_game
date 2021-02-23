893ce74f7f05d29ab5eedb0afbd40bdbbd2c20c3
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-02-23 14:15:19 +1300

    Strat GUI Iteration part 1 video complete

--- 02_start_GUI.py
+++ 02_start_GUI.py
@@ -33,7 +33,7 @@ class Start:
         # Buttons go here..
         button_font = "Arial 12 bold"
         # Orange low stakes button...
-        self.lowstakes_button = Button(text="Low ($5)", command=lambda: self.to_game(1),
+        self.lowstakes_button = Button(self.stakes_frame, text="Low ($5)", command=lambda: self.to_game(1),
                                        font=button_font, bg="#FF9933")
         self.lowstakes_button.grid(row=0, column=0, pady=10)
 
@@ -45,64 +45,23 @@ class Start:
 
         # Green high stakes button...
         self.lowstakes_button = Button(self.stakes_frame, text="High ($15)", command=lambda: self.to_game(3),
-                                       font=button_font, bg="")
+                                       font=button_font, bg="#99FF33")
         self.lowstakes_button.grid(row=0, column=2, pady=10)
 
+        # Help Button
+        self.help_button = Button(self.start_frame, text="How to Play", bg="#808080", fg="white",
+                                  font=button_font)
+        self.help_button.grid(row=4, pady=10)
 
     def to_game(self, stakes):
         starting_balance = self.start_amount_entry.get()
         Game(self, stakes, starting_balance)
 
 
-if __name__ == '__main__':
-    if __name__ == '__main__':
-        class Game:
-            def __init__(self, partner, stakes, starting_balance):
-                print(stakes)
-                print(starting_balance)
-
-                partner.lowstakes_button.config(state=DISABLED)
-
-                # initialise variables
-                self.balance = IntVar()
-
-                # Set starting balance to amount entered by the user at the start if the game
-                self.balance.set(starting_balance)
-
-                # GUI Setup
-                self.game_box = Toplevel()
-                self.game_frame = Frame(self.game_box)
-                self.game_frame.grid()
-
-                # Heading Row
-                self.heading_label = Label(self.game_frame, text="Heading", font="Arial 24 bold",
-                                           padx=10, pady=10)
-                self.heading_label.grid(row=0)
-
-                # Balance Label
-                self.balance_frame = Frame(self.game_frame)
-                self.balance_frame.grid(row=1)
-
-                self.balance_label = Label(self.game_frame, text="Balance...")
-                self.balance_label.grid(row=2)
-
-                self.play_button = Button(self.game_frame, text="Gain", padx=10, pady=10, command=self.reveal_boxes)
-                self.play_button.grid(row=3)
-
-            if __name__ == '__main__':
-                def reveal_boxes(self):
-                    # retrieve of the balance from the initial function..
-                    current_balance = self.balance.get()
-
-                    # Adjust the balance from the initial function..
-                    # For testing purposes, just add 2
-                    current_balance += 2
-
-                    # Set balance to adjusted balance
-                    self.balance.set(current_balance)
-
-                    # Edit label so user can see their balance
-                    self.balance_label.configure(text="Balance: {}".format(current_balance))
+class Game:
+    def __init__(self, partner, stakes, starting_balance):
+        print(stakes)
+        print(starting_balance)
 
 
 # main routine
