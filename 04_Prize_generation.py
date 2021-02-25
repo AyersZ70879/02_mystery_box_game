7e5fa8b7f08d81182a8ccf1b32394d5f8b155920
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-02-26 10:54:06 +1300

    Finsidhed Game Iteration #2 - Background error present

---
+++ 04_Prize_generation.py
@@ -0,0 +1,40 @@
+# Increased trials and showed stats
+
+import random
+
+NUM_TRIALS = 100
+winnings = 0
+
+cost = NUM_TRIALS * 3
+
+for item in range(0, NUM_TRIALS):
+    # prize = ""
+    round_winnings = 0
+
+    for thing in range(0, 3):
+
+        # randint finds numbers between given endpoints including both endpoints
+        prize_num = random.randint(1, 100)
+
+        if 0 < prize_num <= 5:
+            round_winnings += 5
+        elif 5 < prize_num <= 25:
+            round_winnings += 2
+        elif 25 < prize_num <= 65:
+            # prize += "copper"
+            round_winnings += 1
+
+        """else:
+            # prize += "lead"
+            round_winnings += 0"""
+
+    # print("You won {} which is worth {}".format(prize, round_winnings))
+    winnings += round_winnings
+
+print("Paid In: ${}".format(cost))
+print("Pay Out: ${}".format(winnings))
+
+if winnings > cost:
+    print("You came out ${} ahead".format(winnings - cost))
+else:
+    print("Sorry, you lost ${}".format(cost - winnings))
